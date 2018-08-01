8e581ace48afd1f46097f446054fc4e1b6b20cd3
gw <2132>
2018-08-01 14:03:56 +0800

    8.1 2

--- 1.sh
+++ 1.sh
@@ -5,7 +5,7 @@
 #!/usr/bin/env sh
 #set -e用于设置当命令以非零状态退出时，则退出shell。
 #主要作用是，当脚本执行出现意料之外的情况时，立即退出，避免错误被忽略，导致最终结果不正确
-#set -e
+set -e
 echo "Enter release version: "
 read VERSION                                                     #输入版本号
 
@@ -13,7 +13,9 @@ read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r       #输入一个�
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]                                        #判断输入的值是否是Y或y
 then
-  echo "Releasing $VERSION ..."
+  echo "------------------------"
+  echo "|Releasing $VERSION ...|"
+  echo "------------------------"
 
   # build
   #VERSION=$VERSION npm run dist                                  #不清楚
@@ -30,14 +32,18 @@ then
   #fi
   #cd ../..                                                       #回到主目录
   # commit
-  echo "committing"
+  echo "------------------------"
+  echo "|committing............|"
+  echo "------------------------"
   git add -A                                                     #git add .和git add -u的集合
   git commit -m "[build] $VERSION"                               #提交
   npm version $VERSION --message "[release] $VERSION"            #修改当前文件夹下package.json中version的值
 
 
   # publish
-  commit "publishing"
+  echo "------------------------"
+  exho "|publishing............|"
+  echo "------------------------"
   git push origin master                                          #将本地master代码提交到远程master上
   git push origin refs/tags/v$VERSION                             #将本地master代码提交到一个新的远程分支上
   git checkout dev                                               #切换dev分支
