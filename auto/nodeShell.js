0b12920ee8a5b747e54227981a015bd98ae1550d
gw <2132>
2018-08-03 13:14:19 +0800

    8.3 1

--- auto/nodeShell.js
+++ auto/nodeShell.js
@@ -2,11 +2,12 @@ const shell = require('shelljs');
 
 let version = undefined;
 process.stdin.setEncoding('utf8');
+console.log('请输入版本号:');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
     version = chunk;
-    let _version = _version.replace(/^beta/,'');
+    let _version = version.replace(/^beta/,'');
     console.log("git checkout master:",shell.exec('git checkout master').code);
     console.log("git merge dev:",shell.exec('git merge dev').code);
     console.log('开始发布版本v' + version);
