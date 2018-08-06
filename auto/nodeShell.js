7959bfab58688e8ee7b19b9994f7e83885a1ce38
gw <2132>
2018-08-06 09:44:55 +0800

    Merge branch 'dev'
    # Conflicts:
    #	auto/nodeShell.js

--- auto/nodeShell.js
+++ auto/nodeShell.js
@@ -54,6 +54,7 @@ process.stdin.on('end', () => {
   shellExec('git rebase master');
   shellExec('git push origin dev');
   shellExec('npm config set registry http://192.168.0.236:8081/repository/djcpsnpm-host/');
+  shellExec('npm info underscore');
   if(/^beta/.exec(version)){
     shellExec('npm publish --tag beta');
   } else {
