3796d0d6e9839ad6072cc7e720b16fd1f51ca3b4
gw <2132>
2018-08-15 11:12:41 +0800

    修改nodeshell

--- auto/nodeShell.js
+++ auto/nodeShell.js
@@ -37,8 +37,8 @@ process.stdin.on('end', () => {
   // let _version = version.replace(/^beta/, '');
   console.log('开始发布版本v' + version);
   let allBranchLeastCommit = getRemoteBranchHashAndMsg();
-  console.error(getObjValue(allBranchLeastCommit,'/remotes\/\\S+\/master/'));
-  console.error(getObjValue(allBranchLeastCommit,'/remotes\/\\S+\/dev/'));
+  console.error(getObjValue(allBranchLeastCommit,'remotes\/\\S+\/master'));
+  console.error(getObjValue(allBranchLeastCommit,'remotes\/\\S+\/dev'));
   shellExec('git checkout master', {}, () => {
     resetArr.push(`git checkout dev`);
     // masterCommitObj = getHashAndMsg('master');
